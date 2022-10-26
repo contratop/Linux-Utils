b97e00a695f59f3ccb0db69eaa68353ac9e00f72
ContratopDev <miandroidtabletfirst@gmail.com>
2022-10-26 21:27:59 +0200

    Add files via upload

---
+++ powershell-install.debian.sh
@@ -0,0 +1,13 @@
+# Install system components
+sudo apt update  && sudo apt install -y curl gnupg apt-transport-https
+
+# Import the public repository GPG keys
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+# Register the Microsoft Product feed
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list'
+
+# Install PowerShell
+sudo apt update && sudo apt install -y powershell
+
+echo "PowerShell has been installed. You can launch it with the pwsh command."
